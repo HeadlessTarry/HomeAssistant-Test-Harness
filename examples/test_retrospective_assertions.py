ee6e8fe5689ef379b50b171e7341645fd75a1f84
Mark Tarry <mark@thetarry.co.uk>
2026-09-07 19:44:34 +0100

    [fix]: set state before capturing time in retrospective assertion tests
    Tests were capturing current time before setting state, causing History
    API to return pre-change state. Reorder to set state first.

--- examples/test_retrospective_assertions.py
+++ examples/test_retrospective_assertions.py
@@ -55,14 +55,14 @@ class TestRetrospectiveAssertions:
 
     def test_full_duration_mode(self, home_assistant: HomeAssistant, time_machine: TimeMachine) -> None:
         """Test full-duration mode: entity remained in expected state throughout window."""
+        home_assistant.set_state(self.an_entity, "on", {"brightness": 255})
+
         current_time = home_assistant.ws_time_get()
         current_hour = int(current_time["timestamp"][11:13])
         current_minute = int(current_time["timestamp"][14:16])
 
         start_hour, start_minute = current_hour, current_minute
         end_hour, end_minute = _add_minutes(current_hour, current_minute, 3)
-
-        home_assistant.set_state(self.an_entity, "on", {"brightness": 255})
 
         time_machine.fast_forward(timedelta(minutes=1))
         time_machine.fast_forward(timedelta(minutes=3))
@@ -77,14 +77,14 @@ class TestRetrospectiveAssertions:
 
     def test_attribute_matching(self, home_assistant: HomeAssistant, time_machine: TimeMachine) -> None:
         """Test retrospective assertion with attribute matching."""
+        home_assistant.set_state(self.an_entity, "on", {"brightness": 128, "color_temp": 4000})
+
         current_time = home_assistant.ws_time_get()
         current_hour = int(current_time["timestamp"][11:13])
         current_minute = int(current_time["timestamp"][14:16])
 
         start_hour, start_minute = current_hour, current_minute
         end_hour, end_minute = _add_minutes(current_hour, current_minute, 3)
-
-        home_assistant.set_state(self.an_entity, "on", {"brightness": 128, "color_temp": 4000})
 
         time_machine.fast_forward(timedelta(minutes=1))
         time_machine.fast_forward(timedelta(minutes=3))
